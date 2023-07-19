368d46b7456a0ce2355d9ec7d4f40bab5b39048d
Developer <abc@xyz.com>
2023-07-19 16:39:48 +0530

    added base config

---
+++ .eslintrc.js
@@ -0,0 +1,61 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:import/electron',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', 'jsx-a11y', 'simple-import-sort', 'material-ui', 'prettier'],
+  ignorePatterns: ['.history/*', 'webpack/*.js'],
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx', '.js', '.jsx'] }],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'react/prop-types': 0,
+    'react/display-name': 0,
+    'react/react-in-jsx-scope': 0,
+    '@typescript-eslint/ban-types': 1,
+    'prettier/prettier': 'warn',
+    'prefer-const': ['warn', { destructuring: 'all' }],
+    'simple-import-sort/imports': 'warn',
+    'simple-import-sort/exports': 'warn',
+    '@typescript-eslint/ban-ts-comment': 1,
+    'linebreak-style': 0,
+    'react/jsx-sort-props': [
+      'warn',
+      {
+        callbacksLast: true,
+        shorthandFirst: true,
+        ignoreCase: true,
+        reservedFirst: true,
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+      },
+    },
+    'import/ignore': ['.history'],
+    react: {
+      version: 'detect',
+    },
+  },
+};
---
+++ babel.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
+  plugins: [
+    '@babel/plugin-transform-react-jsx',
+    '@babel/plugin-proposal-optional-chaining',
+    [
+      'babel-plugin-import',
+      {
+        libraryName: '@mui/material',
+        libraryDirectory: '',
+        camel2DashComponentName: false,
+      },
+      'core',
+    ],
+    [
+      'babel-plugin-import',
+      {
+        libraryName: '@mui/icons-material',
+        libraryDirectory: '',
+        camel2DashComponentName: false,
+      },
+      'icons',
+    ],
+  ],
+};
