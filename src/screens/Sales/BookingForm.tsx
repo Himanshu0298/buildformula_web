d2c5605bbc94eb14ce067429ba0da4d928807437
yash21563 <jaiswalyashwardhan20@gmail.com>
2023-10-04 19:55:40 +0530

    tofixed value

--- src/screens/Sales/BookingForm.tsx
+++ src/screens/Sales/BookingForm.tsx
@@ -1827,10 +1827,10 @@ const BookingForm = () => {
                       readOnly
                       className="form-control"
                       type="number"
-                      value={
-                       ( parseFloat(handleTotalOtherDiscountAmt()) +
-                        parseFloat(values.basic_rate_disc_amt)).toFixed(2)
-                      }
+                      value={(
+                        parseFloat(handleTotalOtherDiscountAmt()) +
+                        parseFloat(values.basic_rate_disc_amt)
+                      ).toFixed(2)}
                     />
                   </div>
                 </div>
@@ -1864,10 +1864,10 @@ const BookingForm = () => {
                       readOnly
                       className="form-control"
                       type="number"
-                      value={
-                       ( parseFloat(values.basic_rate_basic_amount) +
-                        parseFloat(handleTotalOtherCharge())).toFixed(2)
-                      }
+                      value={(
+                        parseFloat(values.basic_rate_basic_amount) +
+                        parseFloat(handleTotalOtherCharge())
+                      ).toFixed(2)}
                     />
                   </div>
                 </div>
