4b590a28649b0f05a6e1045777b419d95a9b7816
Himanshu Nanikwal <106006151+Himanshu0298@users.noreply.github.com>
2023-10-04 14:30:57 +0530

    navigation

--- src/screens/Sales/BookingForm.tsx
+++ src/screens/Sales/BookingForm.tsx
@@ -1234,6 +1234,7 @@ const BookingForm = () => {
             renderer={props => <Timer {...props} />}
             onComplete={() => {
               localStorage.clear();
+              navigate(-2)
               // window.location.replace('https://google.com');
               // url to be redirect or use navigate to navigate back after submission or after timeout
             }}
